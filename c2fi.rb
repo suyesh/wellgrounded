8a477d034abaea692163dd7e884b8bd95d29febb
Suyesh Bhandari <suyeshb@gmail.com>
2016-03-29 11:26:10 -0400

    .

--- c2fi.rb
+++ c2fi.rb
@@ -1,6 +1,2 @@
-print "Hello. Please enter a Celsius value: "
-celsius = gets
-fahrenheit = (celsius.to_i * 9/5) + 32
-print "The Fahrenheit equivalent is "
-print fahrenheit
-puts "."
+print "Hello, Please enter Celsius value: "
+print "The Fahrenheit equivalent is ", gets.to_i * 9/5 + 32, ".\n"
---
+++ eflag.rb
@@ -0,0 +1,1 @@
+"puts 'Suyesh Bhandari'.reverse"
