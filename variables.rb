9dd288c404e92d0105d0060401977f356bd14249
Suyesh Bhandari <suyeshb@gmail.com>
2016-03-31 12:45:14 -0400

    chapter 2.5

---
+++ variables.rb
@@ -0,0 +1,13 @@
+def say_goodbye
+  x = "Goodbye"
+  puts x
+end
+def start_here
+  x = "Hello"
+  puts x
+  say_goodbye
+  puts "Lets check whether x remained the same: "
+  puts x
+end
+
+start_here
