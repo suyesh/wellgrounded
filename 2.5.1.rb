a805a3213a8fb9de9aed7de57579cce64f1c267a
Suyesh Bhandari <suyeshb@gmail.com>
2016-03-31 13:23:36 -0400

    2.5.3

---
+++ 2.5.1.rb
@@ -0,0 +1,13 @@
+str = "Hello"
+abc = str
+str.replace("Goodbye")
+puts str
+puts abc
+def say_goodbye
+  str = "Hello"
+  abc = str
+  str.replace("Goodbye")
+  puts str
+  puts abc
+end
+say_goodbye
