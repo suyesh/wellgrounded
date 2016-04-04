68315046943ce19dcc114b80debebc0350bc22a7
Suyesh Bhandari <suyeshb@gmail.com>
2016-04-04 14:04:23 -0400

    /

---
+++ 3.2.1.rb
@@ -0,0 +1,21 @@
+class Ticket
+  def initialize(venue, date)
+    @venue = venue
+    @date = date
+    puts "Created a new ticket! for #{@venue}, at #{@date}"
+  end
+
+  def venue
+    @venue
+  end
+
+  def date
+    @date
+  end
+end
+
+th = Ticket.new("NY", "03/01/2017")
+cc = Ticket.new("Conventon hall", "12/13/2015")
+puts "We've create two tickets"
+puts "the first for a #{th.venue} event on #{th.date}."
+puts "The second is for an event on #{cc.date} at #{cc.venue}."
