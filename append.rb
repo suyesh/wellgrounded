a3fd31b9203bf5f07b0021d48f87b9fdb4fc6cdd
Suyesh Bhandari <suyeshb@gmail.com>
2016-05-01 00:03:03 -0400

    .

--- append.rb
+++ append.rb
@@ -1,12 +1,13 @@
-def reverse_append(ary, n)
-  return ary if n < 0
-  reverse_append(ary, n-1)
-  ary << n
-  ary
+def append_non_recursive(arr, n)
+    # puts n downto 0 in arr
+    n.downto(0) { |i| arr << i }
+    arr
 end
 
-sum_upto(4) + 5  = 9
-sum_upto(3) + 5  = 8
-sum_upto(2) + 5  = 7
-sum_upto(1) + 5  = 6
-sum_upto(1) + 5  = 5
+def append_recursive(arr, n)
+    # puts n downto 0 in arr
+    append_non_recursive(arr, n)
+end
+
+puts append_recursive([], 2).inspect
+puts append_recursive([], 3).inspect
