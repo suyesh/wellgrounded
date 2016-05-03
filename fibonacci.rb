20c10926121438e3b763bd4e92a7065ec9c6889d
Suyesh Bhandari <suyeshb@gmail.com>
2016-05-02 21:35:48 -0400

    recursion

--- fibonacci.rb
+++ fibonacci.rb
@@ -7,3 +7,17 @@ end
 
 puts fib(0) #=> 0
 puts fib(10) #=> 55
+
+
+#fib(5)
+   #checks if the n is 0 or 1 and returns it. Else it goes to the next line where it finds the method being called again
+     #fib(5 -1 = 4 ) + fib( 5 - 2 = 3)
+            /                     \
+           /
+        #checks if the n is 0 or 1 and returns it. Else it goes to the next line where it finds the method being called again
+          #fib(4 - 1 = 3) + fib(4 - 2 = 2)
+
+                                        \
+                                         \
+                                    #checks if the n is 0 or 1 and returns it. Else it goes to the next line where it finds the method being called again
+                                      #fib(3 - 1) + fib(3- 2)
